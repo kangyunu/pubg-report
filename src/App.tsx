a8eacebc66295c522eaa6c2471a2afcc4fd64a93
matteo <byungseon.kang@gmail.com>
2026-03-08 20:48:16 +0900

    feat: test api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import RootProvider from "./components/providers/RootProvider";
+
 const App = () => {
-  return <h1> Hello Vite + React! </h1>;
+  const { data: matches } = useQuery({
+    queryKey: ["matches"],
+    queryFn: async () => {
+      const host = "https://kangyunu.github.io/pubg-report";
+      const files = await fetch(`${host}/matches/files.json`);
+      if (!files.ok) {
+        throw new Error("Failed to fetch matches files");
+      }
+      const fileNames: string[] = await files.json();
+
+      const allMatches: Match[] = [];
+      for (const fileName of fileNames) {
+        const res = await fetch(`${host}/matches/${fileName}`);
+        if (!res.ok) {
+          console.warn(`Failed to fetch ${fileName}, skipping.`);
+          continue;
+        }
+        const matches: Match[] = await res.json();
+        allMatches.push(...matches);
+      }
+      return allMatches;
+    },
+  });
+
+  return (
+    <RootProvider>
+      <pre>{JSON.stringify(matches, null, 2)}</pre>{" "}
+    </RootProvider>
+  );
 };
 
 export default App;
